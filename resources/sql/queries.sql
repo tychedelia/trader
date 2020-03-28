583fa4e2183b7674c2792b013571e0cc68003852
James McElwain <jmcelwain@gmail.com>
2020-03-28 12:37:33 -0700

    Fix tests from template.

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,8 +1,8 @@
 -- :name create-user! :! :n
 -- :doc creates a new user record
 INSERT INTO users
-    (id, first_name, last_name, email, pass)
-VALUES (:id, :first_name, :last_name, :email, :pass);
+    (first_name, last_name, email, pass)
+VALUES (:first_name, :last_name, :email, :pass);
 
 -- :name update-user! :! :n
 -- :doc updates an existing user record
--- test/clj/trader/test/db/core.clj
+++ test/clj/trader/test/db/core.clj
@@ -22,12 +22,11 @@
     (jdbc/db-set-rollback-only! t-conn)
     (is (= 1 (db/create-user!
               t-conn
-              {:id         "1"
-               :first_name "Sam"
+              {:first_name "Sam"
                :last_name  "Smith"
                :email      "sam.smith@example.com"
                :pass       "pass"})))
-    (is (= {:id         "1"
+    (is (= {:id         1
             :first_name "Sam"
             :last_name  "Smith"
             :email      "sam.smith@example.com"
@@ -35,4 +34,4 @@
             :admin      nil
             :last_login nil
             :is_active  nil}
-           (db/get-user t-conn {:id "1"})))))
+           (db/get-user t-conn {:id 1})))))
